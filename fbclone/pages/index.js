49b2b768fc70006909100c2db35bce288c7a40f3
Vitaliy Hureyev <hbeatprod@gmail.com>
2021-08-31 19:20:29 +0300

    implement auth

--- fbclone/pages/index.js
+++ fbclone/pages/index.js
@@ -1,20 +1,32 @@
 import Head from 'next/head'
 import Header from "../components/Header";
+import {getSession} from "next-auth/client";
+import Login from "../components/Login";
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Facebook</title>
-      </Head>
-        {/*Header*/}
-        <Header/>
+export default function Home({session}) {
+    if (!session) return <Login/>;
+    return (
+        <div>
+            <Head>
+                <title>Facebook</title>
+            </Head>
+            {/*Header*/}
+            <Header/>
 
-        <main>
-            {/*Sidebar*/}
-            {/*Feed*/}
-            {/*Widgets*/}
-        </main>
-    </div>
-  )
+            <main>
+                {/*Sidebar*/}
+                {/*Feed*/}
+                {/*Widgets*/}
+            </main>
+        </div>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+    return {
+        props: {
+            session
+        }
+    }
 }
