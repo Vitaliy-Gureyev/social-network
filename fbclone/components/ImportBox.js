6c5fa7507eea580d9a2b331ec3bc82c00ff7ff3f
Vitaliy Hureyev <hbeatprod@gmail.com>
2021-09-02 21:14:53 +0300

    connection Firebase and implement posting the post

--- fbclone/components/ImportBox.js
+++ fbclone/components/ImportBox.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Image from "next/image";
 import {useSession} from "next-auth/client";
 import {CameraIcon, EmojiHappyIcon, VideoCameraIcon} from "@heroicons/react/solid";
+import {db, storage} from "../firebase";
+import firebase from 'firebase/compat/app';
 
 const ImportBox = () => {
     const [session] = useSession()
+    const inputRef = useRef(null)
+    const filePickerRef = useRef(null)
+    const [imageToPost, setImageToPost] = useState(null)
 
     const sendPost = (e) => {
         e.preventDefault()
+        if (!inputRef.current.value) return;
+        db.collection('post').add({
+            message: inputRef.current.value,
+            name: session.user.name,
+            email: session.user.email,
+            image: session.user.image,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }).then(doc => {
+            if (imageToPost) {
+                const uploadTask = storage.ref(`posts/${doc.id}`).putString(imageToPost, 'data_url')
+                removeImage()
+
+                uploadTask.on(
+                    'state_change',
+                    null,
+                    error => console.error(error),
+                    () => {
+                        storage.ref(`posts`).child(doc.id).getDownloadURL().then(url => {
+                            db.collection('posts').doc(doc.id).set({
+                                postImage: url
+                            }, {merge: true})
+                        })
+                    })
+            }
+        })
+        inputRef.current.value = ''
+    }
+    const addImageToPost = (e) => {
+        const reader = new FileReader()
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0])
+        }
+        reader.onload = (readerEvent) => {
+            setImageToPost(readerEvent.target.result)
+        }
+    }
+
+    const removeImage = () => {
+        setImageToPost(null)
     }
 
     return (
@@ -18,10 +62,19 @@ const ImportBox = () => {
                        layout="fixed"
                        alt='#'/>
                 <form className='flex flex-1'>
-                    <input className='rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none' type="text"
-                           placeholder={`what's on your mind, ${session.user.name}?`}/>
+                    <input className='rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none'
+                           type="text"
+                           placeholder={`what's on your mind, ${session.user.name}?`}
+                           ref={inputRef}/>
                     <button hidden type='submit' onClick={sendPost}>submit</button>
                 </form>
+                {imageToPost && (
+                    <div
+                        className='flex flex-col filter hover:brightness-110 transform duration-105 cursor-pointer'>
+                        <img className='h-10 object-contain' src={imageToPost} alt="#"/>
+                        <p onClick={removeImage} className='text-xs text-red-500 text-center'>remove</p>
+                    </div>
+                )}
             </div>
 
             <div className='flex justify-evenly p-3 border-t'>
@@ -29,9 +82,12 @@ const ImportBox = () => {
                     <VideoCameraIcon className='h-7 text-red-500'/>
                     <p className='text-xs sm:text-sm xl:text-base'>Live video</p>
                 </div>
-                <div className='inputIcon'>
+                <div onClick={() => {
+                    filePickerRef.current.click()
+                }} className='inputIcon'>
                     <CameraIcon className='h-7 text-green-400'/>
                     <p className='text-xs sm:text-sm xl:text-base'>Photo/Video</p>
+                    <input ref={filePickerRef} type="file" hidden onChange={addImageToPost}/>
                 </div>
                 <div className='inputIcon'>
                     <EmojiHappyIcon className='h-7 text-yellow-300'/>
---
+++ fbclone/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA10jgWgc6ZAS8hfdZCTQvypGuiYobkvf0",
+    authDomain: "facebook-a8644.firebaseapp.com",
+    projectId: "facebook-a8644",
+    storageBucket: "facebook-a8644.appspot.com",
+    messagingSenderId: "777080784321",
+    appId: "1:777080784321:web:fbe5a253e078c4075a1b40"
+};
+
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app;
+const db = app.firestore()
+const storage = firebase.storage()
+
+export {db, storage}
